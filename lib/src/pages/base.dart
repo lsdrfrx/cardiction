654ad74d0e934ae6c3a71a05e1dbe7401ef6efec
Christian Guetnga <lsdrfrx@gmail.com>
2023-10-04 19:18:57 +0300

    added base page template

---
+++ lib/src/pages/base.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+import "package:flutter_desktop_test/src/widgets/appbar.dart";
+import "package:flutter_desktop_test/src/widgets/drawer.dart";
+
+class BasePage extends StatelessWidget {
+  final Widget body;
+  const BasePage({super.key, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      drawer: const CustomDrawer(),
+      body: body,
+    );
+  }
+}
---
+++ lib/src/pages/settings.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import "package:flutter_desktop_test/src/pages/base.dart";
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BasePage(
+      body: Container(),
+    );
+  }
+}
