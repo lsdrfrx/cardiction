cae82445ed3d5b4c4a5927340641e929bd882de3
Anastasiya Zinovyeva <zinnastya99@mail.ru>
2023-10-04 19:49:18 +0300

    implementing new card page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_desktop_test/src/pages/new_card.dart';
 import 'package:flutter_desktop_test/src/pages/vocabulary.dart';
 
 void main() {
   const app = MaterialApp(
-    home: Vocabulary(),
+    home: NewCardPage(),
   );
 
   runApp(app);
--- lib/src/pages/new_card.dart
+++ lib/src/pages/new_card.dart
@@ -1,15 +1,58 @@
 import "package:flutter/material.dart";
+import "package:flutter_desktop_test/src/widgets/appbar.dart";
+import "package:flutter_desktop_test/src/widgets/drawer.dart";
 
 class NewCardPage extends StatelessWidget {
   const NewCardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        border: Border.all(color: Colors.black54, width: 2.0),
-      )
+    return Scaffold(
+      appBar: CustomAppBar(),
+      drawer: const CustomDrawer(),
+      body: Column(
+        children: [
+
+          const SizedBox(height: 30),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            margin: const EdgeInsets.only(left: 30, right: 30),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.black54, width: 4.0),
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            margin: const EdgeInsets.only(left: 30, right: 30),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue[50],
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.black54, width: 4.0),
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          Container(
+            // width: MediaQuery.of(context).size.width - 60,
+            height: 60,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black54, width: 4),
+            ),
+            child: const Center(
+              child: Text('Save card', style: TextStyle(fontSize: 30),),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
